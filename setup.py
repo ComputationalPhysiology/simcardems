8b3f806158993d59c7b696a71cd37735ec4fdc47
Henrik Finsberg <henriknf@simula.no>
2021-10-23 18:27:04 +0200

    Fix parsing of version

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 """The setup script."""
+from pathlib import Path
+
 from setuptools import setup
 
-setup()
+_here = Path(__file__).parent
+
+with open(
+    _here.joinpath("src").joinpath("simcardems").joinpath("version.py"),
+    "r",
+) as f:
+    version = f.read().split('"')[1]
+
+
+setup(version=version)
