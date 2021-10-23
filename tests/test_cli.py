e3f533f9ebd98241c75f33d64039777a2b1018db
Henrik Finsberg <henriknf@simula.no>
2021-10-23 17:58:58 +0200

    Test that correct parameters are used in the cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,30 +11,40 @@ mesh_args = ["-lx", 1, "-ly", 1, "-lz", 1, "-dx", 1]
 
 def test_run():
     runner = CliRunner()
-    outdir = Path("test_outdir")
+    outdir = Path("test_outdir").absolute()
     if outdir.exists():
         shutil.rmtree(outdir)
-    result = runner.invoke(run, ["-T", 1, "-o", outdir.as_posix()] + mesh_args)
+    args = ["-T", 1, "--outdir", outdir.as_posix()] + mesh_args
+    kwargs = dict(zip([arg.strip("-").strip("-") for arg in args[::2]], args[1::2]))
+    result = runner.invoke(run, args)
     assert result.exit_code == 0
     assert outdir.exists()
     assert outdir.joinpath("results.h5").is_file()
+
+    with open(outdir.joinpath("parameters.json"), "r") as f:
+        dumped_arguments = json.load(f)
+
+    for k, v in kwargs.items():
+        assert dumped_arguments[k] == v, k
 
 
 def test_run_json():
     runner = CliRunner()
-    outdir = Path("test_outdir")
+    outdir = Path("test_outdir").absolute()
     if outdir.exists():
         shutil.rmtree(outdir)
 
     args = ["-T", 1, "--outdir", outdir.as_posix()] + mesh_args
-    data = dict(zip([arg.strip("-").strip("-") for arg in args[::2]], args[1::2]))
+    kwargs = dict(zip([arg.strip("-").strip("-") for arg in args[::2]], args[1::2]))
     json_path = "args.json"
 
     with open(json_path, "w") as jsonfile:
-        json.dump(data, jsonfile)
+        json.dump(kwargs, jsonfile)
     result = runner.invoke(run_json, [json_path])
-    # breakpoint()
-    print(result.output)
     assert result.exit_code == 0
     assert outdir.exists()
     assert outdir.joinpath("results.h5").is_file()
+    with open(outdir.joinpath("parameters.json"), "r") as f:
+        dumped_arguments = json.load(f)
+    for k, v in kwargs.items():
+        assert dumped_arguments[k] == v
