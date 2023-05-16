76fd678517600bc6fe260980dc29e2b5c8816c98
Henrik Finsberg <henriknf@simula.no>
2023-05-16 10:31:55 +0200

    Merge pull request #181 from ComputationalPhysiology/petsc-memory-issue
    Fix PETSc memory issue

--- src/simcardems/models/explicit_ORdmm_Land/em_model.py
+++ src/simcardems/models/explicit_ORdmm_Land/em_model.py
@@ -93,10 +93,15 @@ class EMCoupling(em_model.BaseEMCoupling):
         """Interpolates function from mechanics to ep mesh"""
 
         x = dolfin.as_backend_type(f_mech.vector()).vec()
-        _, temp = self.transfer_matrix.getVecs()
+        a, temp = self.transfer_matrix.getVecs()
         self.transfer_matrix.mult(x, temp)
         f_ep.vector().vec().aypx(0.0, temp)
         f_ep.vector().apply("")
+
+        # Remember to free memory allocated by petsc: https://gitlab.com/petsc/petsc/-/issues/1309
+        x.destroy()
+        a.destroy()
+        temp.destroy()
 
     def __eq__(self, __o: object) -> bool:
         if not isinstance(__o, type(self)):
